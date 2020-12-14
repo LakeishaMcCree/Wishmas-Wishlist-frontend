fa95d370b725d3960e78e28e6876e344013fa9f6
github username <lmccree215@icloud.comgit config --global user.name LakeishaMcCree>
2020-12-14 01:27:34 -0500

    refactored get request to clean up code

--- javascript/index.js
+++ javascript/index.js
@@ -1,32 +1,9 @@
-const endPoint = "http://localhost:3000/lists"
-
 document.addEventListener("DOMContentLoaded", function() {
-    getLists()
-    // do whatever has to happen first in your application
-    //API.addItems()
-    //for our application, we're going to need to load in our Christmas lists
-    //const createListForm = document.querySelector("#create-list")
+   
+    API.addLists()
     
-    
-    
-    // If we have any event listeners that we need to get apended to the page when it loads, the should get appended here too.
-
 })
 
-function getLists(){
-    fetch(endPoint) //get request
-    .then(resp => resp.json())
-    .then(lists => {
-        lists.forEach(list => {
-            let newList = new List(list)
-            document.querySelector("#wishmas-wishlist-container").innerHTML += newList.renderList()
-        })
-        //addListsToPage(lists);
 
-    })
-}
 
-//function addListstoPage(lists){
-    
-    //lists.forEach(function(list))
-//}
+
--- javascript/services/API.js
+++ javascript/services/API.js
@@ -1,12 +1,13 @@
 class API {
     // static (class level) function that is going to be a fetch request to the back end to the index that will load in all my lists
-    static addItems(){
+    static addLists(){
         fetch("http://localhost:3000/lists") //read fetch
         .then(resp => resp.json())
         .then(lists => {
             lists.forEach(list => {
                 const{id, name, list_notes} = list 
                 new List(id, name, list_notes)
+            debugger
             })
         })
 
