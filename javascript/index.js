6f906caac4d9923af3b1b368d7b6da2c1ea9b378
github username <lmccree215@icloud.comgit config --global user.name LakeishaMcCree>
2020-12-14 02:05:36 -0500

    created a form, added an event listener and event handler to submit form

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
    
-    API.addLists()
+    API.addItems()
+
+
+    const createListForm = document.querySelector("#create-list-form")
+
+    createListForm.addEventListener("submit", (e) => createFormHandler(e))
     
 })
+
+function createFormHandler(e) {
+    e.preventDefault()
+   debugger
+}
 
 
 
