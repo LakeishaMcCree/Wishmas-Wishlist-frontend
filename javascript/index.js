00a0f7ea41b37b73c14cacaa21caca22a86b3efe
github username <lmccree215@icloud.comgit config --global user.name LakeishaMcCree>
2020-12-14 02:34:12 -0500

    added a fetch function

--- javascript/index.js
+++ javascript/index.js
@@ -11,9 +11,18 @@ document.addEventListener("DOMContentLoaded", function() {
 
 function createFormHandler(e) {
     e.preventDefault()
-   debugger
+    const nameInput = document.querySelector('#input-name').value
+    const priceInput = document.querySelector('#input-price').value
+    const numberInput = document.querySelector('#input-number').value
+    const storeInput = document.querySelector('#input-store').value
+    const urlInput = document.querySelector('#input-url').value
+    const imageInput = document.querySelector('#input-image').value
+    postFetch(nameInput, priceInput, numberInput, storeInput, urlInput, imageInput)
 }
 
+function postFetch(name, price, number, store, url, image) {
+    console.log
+}
 
 
 
