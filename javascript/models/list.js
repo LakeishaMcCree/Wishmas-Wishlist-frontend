b61eb426592765d68a324ca4aafe447e1fed1828
github username <lmccree215@icloud.comgit config --global user.name LakeishaMcCree>
2021-01-02 15:35:28 -0500

    created a new list div

--- javascript/models/list.js
+++ javascript/models/list.js
@@ -1,8 +1,8 @@
 class List {
     constructor (id, name, list_notes) {
-        this.id = id
-        this.name = name
-        this.list_notes = list_notes
+        this.id = id;
+        this.name = name;
+        this.list_notes = list_notes;
         this.postList()
         this.fetchItems()
         //render the instance to the page
@@ -12,12 +12,14 @@ class List {
         return `
         <div class="row">
             <div class="col s12 m7">
+            <form id="wishmas-wishlist">
                 <div class="card horizontal blue-grey darken-1">
                     <div class="card-content white-text">
                         <span class="card-title">${this.name}</span>
                         <p> About the Child: ${this.list_notes}</p>
                     <div id="item-container ${this.id}"></div>
                 </div>
+                </form>
                 <div class="card-action">
                     <button type="button" class="item-button" data-id=${this.id}>Edit!</button>
                     <button type="button" class="item-button" data-id=${this.id}>Delete!</button>
@@ -30,11 +32,11 @@ class List {
     //
     //dataset.id- refers to an id, creating and displaying my list
     postList() {
-        const listContainer = document.getElementById("add-item-form");
-        const listCard = document.createElement('div');
+        const listContainer = document.getElementById("list-spot")
+        const listCard = document.createElement('div')
         listCard.dataset.id = this.id 
         listCard.id = this.id
-        listCard.classList.add = "wishmas-wishlist"
+        listCard.classList.add = "list-card"
         listCard.innerHTML += this.listHTML()
         listContainer.appendChild(listCard)
         listCard.addEventListener('click', e => {
@@ -59,9 +61,11 @@ class List {
         fetch(`http://localhost:3000/lists/${this.id}`, {
             method: 'DELETE'
         })
-        .then(() => { //removing the child of the parent div, by its numbered ID corresponding with what's in the database
-            document.getElementById("wishlist-container").removeChild(id)
+        .then(() => {
+            document.getElementById('list-spot').removeChild(id)
         })
+       
+            
     }
         
 }
